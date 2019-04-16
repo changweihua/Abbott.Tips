3082b0862b4aaeefa69395153c40cb7e9d2973ff
changweihua <changweihua@outlook.com>
2019-04-16 18:00:24 +0800

    完善JSON自动转义

---
+++ Abbott.Tips/Abbott.Tips.Core/Mapper/ResultMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using Abbott.Tips.EntityFrameworkCore.Mappers;
+using Abbott.Tips.Model;
+using Abbott.Tips.Model.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abbott.Tips.Core.Mapper
+{
+    public class ResultMapperProfile : AutoMapper.Profile, IMapperProfile
+    {
+        public ResultMapperProfile()
+        {
+            CreateMap(typeof(IPager<>), typeof(JsonListResultModel<>))
+                .ForMember(dest => dest.Items, options => options.MapFrom(src => src.Items));
+        }
+    }
+}
